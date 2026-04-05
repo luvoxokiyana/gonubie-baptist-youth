940d7ab4395ef417559ce3940320bfa7656ef519
luvoxokiyana <luvoxokiyana17@gmail.com>
2026-04-05 16:47:36 +0200

    new file: api.php new file: database.php

---
+++ database.php
@@ -0,0 +1,4 @@
+private $host = 'sql100.infinityfree.com';     // Your database host
+private $db_name = 'church_youth';  // Database name
+private $username = 'if0_41572398';      // Your database username
+private $password = 'GuyGam3r20';          // Your database password
