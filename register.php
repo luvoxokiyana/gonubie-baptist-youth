7183eeca690fb483a8dff0654a11203ef59829cb
luvoxokiyana <luvoxokiyana17@gmail.com>
2026-04-08 13:31:09 +0200

    style update: registar.php

--- register.php
+++ register.php
@@ -49,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Account - Gonubie Baptist Youth</title>
-    <link rel="stylesheet" href="css/main.css?v=1.0">
+    <link rel="stylesheet" href="css/main.css?v=2.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
         .register-container {
